f489e6783b290c9c36fee5fcd7dc8b5e14cac3e3
arun kesavan <arunkottilukkal@gmail.com>
2020-07-30 22:21:43 +0530

    Final commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-  name = 'AnalyseSentiment',         # How you named your package folder (MyLib)
+  name = 'SentimentAnalysis',         # How you named your package folder (MyLib)
   packages = ['AnalyseSentiment'],   # Chose the same as "name"
   version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
