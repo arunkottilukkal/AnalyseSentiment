5c0c4f5aba311fe275671b2d203c0b50747876f6
arun kesavan <arunkottilukkal@gmail.com>
2020-07-31 12:38:01 +0530

    Final upload

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 setup(
   name = 'SentimentAnalysis',         # How you named your package folder (MyLib)
   packages = ['AnalyseSentiment'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  version = '0.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Helps to analyse sentiment',   # Give a short description about your library
   long_description = long_description,
   author = 'Arun Kesavan',                   # Type in your name
   author_email = 'arunkottilukkal@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/arunkottilukkal/AnalyseSentiment',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/arunkottilukkal/AnalyseSentiment/archive/v_05.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/arunkottilukkal/AnalyseSentiment/archive/v_06.tar.gz',    # I explain this later on
   keywords = ['Sentiment Analysis', 'Sentence Sentiment', 'Emotion Analysis'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
